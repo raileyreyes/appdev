3b65f89249fd11a5297ff26ff0c20bd7a47484e7
Railey Reyes <202410536@fit.edu.ph>
2026-05-18 12:21:56 +0800

    files added; no contents except header.php

---
+++ TFA4/act 1 biography/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>[TFA4] Biography</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Biography of My Friends</h1>
+        <p>Using include() and require() to display the biography of my friends.</p>
+    </header>
+</body>
+</html>
