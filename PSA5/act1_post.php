e2d132008559fc43dcd99f72eb9f20534e6a47a2
Railey Reyes <202410536@fit.edu.ph>
2026-06-15 10:49:14 +0800

    post added; get changes in php

--- PSA5/act1_post.php
+++ PSA5/act1_post.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Activity 1</title>
+    <title>Activity 1: Post</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -11,9 +11,9 @@
 
 <div class="container">
     <h1>Personal Information</h1>
-    <p class="subtitle">Using $_GET Function</p>
+    <p class="subtitle">Using $_POST Function</p>
 
-    <form method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <label> First Name: </label>
         <input type="text" name="firstname" required>
 
@@ -31,20 +31,19 @@
 
         <input type="submit" name="submit" value="Submit">
     </form>
-    
+
     <?php
-        if (isset($_GET['submit'])) {
+        if (isset($_POST['submit'])) {
             echo "<div class='result'>";
             echo "<h2>Submitted Information:</h2>";
-            echo "First Name: " . $_GET['firstname'] . "<br>";
-            echo "Middle Name: " . $_GET['middlename'] . "<br>";
-            echo "Last Name: " . $_GET['lastname'] . "<br>";
-            echo "Date of Birth: " . $_GET['dob'] . "<br>";
-            echo "Address: " . $_GET['address'] . "<br>";
+            echo "First Name: " . $_POST['firstname'] . "<br>";
+            echo "Middle Name: " . $_POST['middlename'] . "<br>";
+            echo "Last Name: " . $_POST['lastname'] . "<br>";
+            echo "Date of Birth: " . $_POST['dob'] . "<br>";
+            echo "Address: " . $_POST['address'] . "<br>";
             echo "</div>";
         }
-    ?>
+        ?>
 </div>
-
 </body>
 </html>
