2540348e6d1a175c4a4de3125411481adafcd3b0
Railey Reyes <202410536@fit.edu.ph>
2026-06-15 11:25:43 +0800

    post and get files compiled to 1 php file.

--- PSA5/act1.php
+++ PSA5/act1.php
@@ -11,6 +11,43 @@
 
 <div class="container">
     <h1>Personal Information</h1>
+    <p class="subtitle">Using $_POST Function</p>
+
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label> First Name: </label>
+        <input type="text" name="firstname" required>
+
+        <label> Middle Name: </label>
+        <input type="text" name="middlename" required>
+
+        <label> Last Name: </label>
+        <input type="text" name="lastname" required>
+
+        <label> Date of Birth: </label>
+        <input type="text" name="dob" required>
+
+        <label> Address:</label>
+        <input type="text" name="address" required>
+
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+        if (isset($_POST['submit'])) {
+            echo "<div class='result'>";
+            echo "<h2>Submitted Information:</h2>";
+            echo "First Name: " . $_POST['firstname'] . "<br>";
+            echo "Middle Name: " . $_POST['middlename'] . "<br>";
+            echo "Last Name: " . $_POST['lastname'] . "<br>";
+            echo "Date of Birth: " . $_POST['dob'] . "<br>";
+            echo "Address: " . $_POST['address'] . "<br>";
+            echo "</div>";
+        }
+        ?>
+</div>
+
+<div class="container">
+    <h1>Personal Information</h1>
     <p class="subtitle">Using $_GET Function</p>
 
     <form method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
